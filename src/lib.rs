7d6f37004eb95e1d942f1a1861ef49439d95e4de
Stuart Haidon <serayen.sh@gmail.com>
2021-06-20 16:36:41 +0100

    The actual changes from the last commit.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use bench::{BenchEvent, MemoryData, RuntimeData};
 use bytesize::ByteSize;
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use console::Term;
+use indicatif::{MultiProgress, ProgressBar, ProgressDrawTarget, ProgressStyle};
 use once_cell::sync::Lazy;
-use rayon::ThreadPoolBuilder;
+use rayon::{ThreadPool, ThreadPoolBuilder};
 use structopt::StructOpt;
 use thiserror::Error;
 
@@ -16,6 +17,9 @@ pub use alloc::TracingAlloc;
 use std::{fmt::Display, iter::once, sync::mpsc::channel, thread, time::Duration};
 
 static ARGS: Lazy<Args> = Lazy::new(Args::from_args);
+
+pub type BenchResult = Result<(), BenchError>;
+type Function = for<'a> fn(&'a str, Bench) -> BenchResult;
 
 #[derive(Debug, Error)]
 pub enum BenchError {
@@ -35,8 +39,6 @@ pub enum BenchError {
     #[error("{}", .0)]
     UserError(#[source] Box<dyn std::error::Error + Send + Sync>),
 }
-
-pub type BenchResult = Result<(), BenchError>;
 
 #[derive(Copy, Clone, StructOpt)]
 pub(crate) enum OutputType {
@@ -142,21 +144,22 @@ pub fn input(year: u16, day: u8) -> InputFile<ProblemInput> {
 pub struct Day {
     pub name: &'static str,
     pub day: u8,
-    pub part_1: for<'a> fn(&'a str, Bench) -> Result<(), BenchError>,
-    pub part_2: Option<for<'a> fn(&'a str, Bench) -> Result<(), BenchError>>,
+    pub part_1: Function,
+    pub part_2: Option<Function>,
 }
 
 struct BenchedFunction {
-    // id: usize,
     // name: &'static str,
-    // day: usize,
+    day: u8,
+    day_function_id: u8,
+    function: Function,
     answer: Option<String>,
     error: Option<String>,
     timing_data: Option<RuntimeData>,
     memory_data: Option<MemoryData>,
     finished_spinner: ProgressStyle,
     error_spinner: ProgressStyle,
-    bar: ProgressBar,
+    bar: Option<ProgressBar>,
 }
 
 fn get_precision(val: Duration) -> usize {
@@ -170,35 +173,45 @@ fn get_precision(val: Duration) -> usize {
 impl BenchedFunction {
     fn answer(&mut self, ans: String) {
         self.answer = Some(ans);
-        self.bar.set_style(self.finished_spinner.clone());
-        let msg = self.render();
-        self.bar.set_message(msg);
+        if let Some(bar) = &self.bar {
+            bar.set_style(self.finished_spinner.clone());
+            let msg = self.render();
+            bar.set_message(msg);
+        }
     }
 
     fn memory(&mut self, data: MemoryData) {
         self.memory_data = Some(data);
-        let msg = self.render();
-        self.bar.set_message(msg);
+        if let Some(bar) = &self.bar {
+            let msg = self.render();
+            bar.set_message(msg);
+        }
     }
 
     fn timing(&mut self, data: RuntimeData) {
         self.timing_data = Some(data);
-        let msg = self.render();
-        self.bar.set_message(msg);
+        if let Some(bar) = &self.bar {
+            let msg = self.render();
+            bar.set_message(msg);
+        }
     }
 
     fn error(&mut self, err: String) {
         self.error = Some(err);
-        self.bar.set_style(self.error_spinner.clone());
-        let msg = self.render();
-        self.bar.set_message(msg);
+        if let Some(bar) = &self.bar {
+            bar.set_style(self.error_spinner.clone());
+            let msg = self.render();
+            bar.set_message(msg);
+        }
     }
 
     fn finish(&mut self) {
-        self.bar.finish()
+        if let Some(bar) = &self.bar {
+            bar.finish()
+        }
     }
 
-    fn render(&mut self) -> String {
+    fn render(&self) -> String {
         if let Some(err) = self.error.as_deref() {
             err.to_string()
         } else {
@@ -309,8 +322,6 @@ pub fn run(alloc: &'static TracingAlloc, year: u16, days: &[Day]) -> Result<(), 
         .build()
         .expect("Failed to build threadpool");
 
-    let (sender, receiver) = channel::<BenchEvent>();
-
     println!("Advent of Code {}", year);
     println!("   Day | {:<30} | {:<10} | Max Mem.", "Answer", "Time");
     println!("_______|_{0:_<30}_|_{0:_<10}_|______________", "");
@@ -325,90 +336,51 @@ pub fn run(alloc: &'static TracingAlloc, year: u16, days: &[Day]) -> Result<(), 
         .clone()
         .template("{spinner} {prefix:.red} | {msg}");
 
-    let bars = MultiProgress::new();
+    // MultiProgress goes a bit nuts if the terminal isn't tall enough to display all the bars
+    // at once. So we need to chunk the functions to bench based on how tall the terminal is.
+    let stdout = Term::stdout();
+    let (rows, _) = stdout.size();
+    // Add room for header and trailing line.
+    let rows = rows.saturating_sub(5) as usize;
 
     let mut benched_functions = Vec::new();
-    let mut id = 0;
+    let mut cur_chunk = Vec::new();
 
     for day in days {
         let parts = once(day.part_1).chain(day.part_2).zip(1..);
 
         for (f, i) in parts {
-            let bar = bars.add(ProgressBar::new(10000));
-            bar.set_prefix(format!("{:2}.{}", day.day, i));
-            bar.set_style(spinner_style.clone());
-            bar.enable_steady_tick(250);
+            if cur_chunk.len() == rows {
+                benched_functions.push(cur_chunk);
+                cur_chunk = Vec::new();
+            }
 
             let p1f = BenchedFunction {
-                // id,
-                // day: day.day,
+                day: day.day,
                 // name: day.name,
+                day_function_id: i,
+                function: f,
                 answer: None,
                 error: None,
                 timing_data: None,
                 memory_data: None,
                 finished_spinner: finished_spinner.clone(),
                 error_spinner: error_spinner.clone(),
-                bar,
+                bar: None,
             };
 
-            benched_functions.push(p1f);
-
-            let bench = Bench {
-                alloc,
-                id,
-                chan: sender.clone(),
-                args: &ARGS,
-            };
-            let sender = sender.clone();
-            let input = input(year, day.day).open()?;
-
-            pool.spawn(move || {
-                if let Err(e) = f(&input, bench) {
-                    sender
-                        .send(BenchEvent::Error {
-                            err: e.to_string(),
-                            id,
-                        })
-                        .expect("Unable to send error");
-
-                    sender
-                        .send(BenchEvent::Finish { id })
-                        .expect("Unable to send finish");
-                }
-            });
-
-            id += 1;
+            cur_chunk.push(p1f);
         }
     }
+    if !cur_chunk.is_empty() {
+        benched_functions.push(cur_chunk);
+    }
 
-    // If we don't drop this thread's sender the handler thread will never stop.
-    drop(sender);
+    let total_time = benched_functions
+        .into_iter()
+        .map(|days_chunk| bench_days_chunk(alloc, year, days_chunk, &spinner_style, &pool))
+        .try_fold(Duration::ZERO, |acc, a| a.map(|a| a + acc))?;
 
-    let (time_sender, time_receiver) = channel::<Duration>();
-    let handler_thread = thread::spawn(move || {
-        for event in receiver.iter() {
-            match event {
-                BenchEvent::Answer { answer, id } => benched_functions[id].answer(answer),
-                BenchEvent::Memory { data, id } => benched_functions[id].memory(data),
-                BenchEvent::Timing { data, id } => {
-                    time_sender
-                        .send(data.mean_run)
-                        .expect("Failed to send timing from handler thread");
-                    benched_functions[id].timing(data);
-                }
-                BenchEvent::Error { err, id } => benched_functions[id].error(err),
-                BenchEvent::Finish { id } => benched_functions[id].finish(),
-            }
-        }
-    });
-
-    bars.join().expect("Failed to join progress bars");
-    handler_thread
-        .join()
-        .expect("Failed to join handler thread");
-
-    let total_time = time_receiver.iter().sum();
     let prec = get_precision(total_time);
     println!("_______|_{0:_<30}_|_{0:_<10}_|______________", "");
     println!(
